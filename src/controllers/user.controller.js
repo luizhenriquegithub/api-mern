d224a11aae68a32c3e6f9fdc01012706553f1cc9
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-09-13 16:42:23 -0300

    refactor config router user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,21 @@
-const soma = (req, res) => {
-  const soma = 100 + 1;
-  res.send({ soma: soma });
+const create = (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name || !username || !email || !password || !avatar || !background) {
+    res.status(400).send({ message: "Sumit all fields for registration" });
+  }
+
+  res.status(201).json({
+    message: "user created success !!!",
+    user: {
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      background,
+    },
+  });
 };
 
-module.exports = { soma };
+module.exports = { create };
