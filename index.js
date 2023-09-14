0852e0615bbbeb6217cbdef51ab22bd791383bd3
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-09-14 14:17:11 -0300

    refactor ordem do express.json

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const connectDataBase = require("./src/database/db");
 const userRoute = require("./src/routes/user.route");
 
 const app = express();
-const port = 3000;
-
-/** conexao com o bonco mongoose */
-connectDataBase();
-
-app.use("/user", userRoute);
 app.use(express.json());
+app.use("/user", userRoute);
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+/** conexao com o bonco mongoose */
+connectDataBase();
