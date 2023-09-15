36793527253de3ea755167e16d322a3e815dfe87
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-09-15 13:51:07 -0300

    add dotenv variaveis de ambiente

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import connectDataBase from "./src/database/db.js";
 import userRoute from "./src/routes/user.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/user", userRoute);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
--- src/database/db.js
+++ src/database/db.js
@@ -4,10 +4,10 @@ const connectDataBase = () => {
   console.log("wait connectiong to the database !!");
 
   mongoose
-    .connect(
-      "mongodb+srv://root:root@cluster0.46ai594.mongodb.net/?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas Connected..."))
     .catch((error) => console.log(error));
 };
