b9e6a0c5ae2a3e06df90d7bd310c1cfa30c6d356
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-09-14 21:13:57 -0300

    add try catch and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,45 +1,59 @@
 const userService = require("../services/user.services");
 
 const create = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
+  try {
+    const { name, username, email, password, avatar, background } = req.body;
 
-  if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({ message: "Sumit all fields for registration" });
+    if (!name || !username || !email || !password || !avatar || !background) {
+      res.status(400).send({ message: "Sumit all fields for registration" });
+    }
+
+    const user = await userService.create(req.body);
+
+    if (!user) {
+      return res.status(400).send({ message: "Error creating User" });
+    }
+
+    res.status(201).json({
+      message: "user created success !!!",
+      user: {
+        id: user._id,
+        name,
+        username,
+        email,
+        password,
+        avatar,
+        background,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-
-  const user = await userService.create(req.body);
-
-  if (!user) {
-    return res.status(400).send({ message: "Error creating User" });
-  }
-
-  res.status(201).json({
-    message: "user created success !!!",
-    user: {
-      id: user._id,
-      name,
-      username,
-      email,
-      password,
-      avatar,
-      background,
-    },
-  });
 };
 
 const getAll = async (req, res) => {
-  const users = await userService.getAll();
+  try {
+    const users = await userService.getAll();
 
-  if (users.length === 0) {
-    return res.status(400).send({ message: "There are not registered users" });
+    if (users.length === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are not registered users" });
+    }
+
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ mesage: error.message });
   }
-
-  res.send(users);
 };
 
 const getById = async (req, res) => {
-  const user = req.user;
-  res.send(user);
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 };
 
 const update = async (req, res) => {
